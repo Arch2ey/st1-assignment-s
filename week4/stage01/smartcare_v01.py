ddf3b4dc638d4f0396f21f4ef0eea3759edc2e90
Archie Toohey <U3331433@uni.canberra.edu.au>
2026-09-07 15:46:17 +1000

    Test week 4 code

--- week4/stage01/smartcare_v01.py
+++ week4/stage01/smartcare_v01.py
@@ -42,3 +42,5 @@ print("Welcome to SmartCare: The Clinical Appointment Booking System!")
 book_appointment('Alice Smith', 'Dr. John Doe', '2024-07-20 10:00 AM')
 book_appointment('Bob Johnson', 'Dr. Jane Roe', '2024-07-20 11:30 AM')
 display_appointments()
+
+# just modifying it to test git commit and push
